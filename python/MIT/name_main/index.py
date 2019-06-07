7b904d28389d1699bffd40f6e455e783311ec31e
Yuta Yamadera <yutawaseda7@outlook.jp>
2019-06-07 22:40:54 +0900

    study function

---
+++ python/MIT/name_main/index.py
@@ -0,0 +1,17 @@
+def main(x):
+    y = 1
+    x = x + y
+    # print("x=", x)
+    return x
+x = 3
+y = 2
+z = main(x)
+print(z)
+print(x)
+print(y)
+
+#   def main():
+#     print("Hello")
+
+# if __name__ == "__main__":
+#     main()
